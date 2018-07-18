94ce05ad6f760849e06631e362f07987ca30ef2d
rtorres <rtorres@lmsonline.com>
2018-07-18 06:51:04 -0400

    moving from one pc to other

--- romanNumeralConverter.js
+++ romanNumeralConverter.js
@@ -1,39 +1,53 @@
 function convertToRoman(num) {
-    
-    // convert input to string
-    let numeral = num.toString();
-    console.log(typeof(numeral));
-    //select characters
-    var number = 0;
-    let len = numeral.length;
-    console.log(len ,'lenth');
+    strNum = num.toString();
+    let romanNumeralsKeeper = [];
+    decimals = [1,2,3,4,5,6,7,8,9];
 
-    
-        if(len === 1){
-           oneDigit(numeral);            
+romanOnes = ["I","II","III","IV","V","VI","VII","VIII","IX"];
+ 								
+// tens =[10,20,30,40,50,60,70,80,90]
+romanTens = ["X","XX","XXX","XL","L","LX","LXX","LXXX","XC"];
+ 								
+// hundred = [100,200,300,400,500,600,700,800,900]
+romanHundred =["C","CC","CCC","CD","D","DC","DCC","DCCC","CM"];
+
+
+switch( strNum.length){
+    case 4:
+        count = 0;
+        while(count < strNum.charAt(0)){
+            romanNumeralsKeeper.push("M")
+            count ++;
         }
-   
+        if(strNum.charAt(1) != 0){
+            romanNumeralsKeeper.push(romanHundred.indexOf(strNum.charAt(1)+1));
 
-
-    // convertion logic
-    console.log(number);
-    
-    
-    function oneDigit(n){
-        let romanNumeral = ["I","II","III","IV","V","VI","VII","VIII","IX"];
-        console.log(romanNumeral.length);
-        for(let i = 0; i <= romanNumeral.length ; i++){
-            // console.log(i);
-            if (numeral == i){
-                console.log(i);
-                number = romanNumeral[i-1];
-                               
-            }
         }
-    }
-   
-    
-    return num;
+        romanNumeralsKeeper.push(romanTens[strNum.charAt(2+1)]);
+        romanNumeralsKeeper.push(romanOnes[strNum.charAt(3+1)]); 
+    break;
+
+    case 3:                
+    romanNumeralsKeeper.push(romanHundred[strNum.charAt(0)]);
+    romanNumeralsKeeper.push(romanTens[strNum.charAt(1)]);
+    romanNumeralsKeeper.push(romanOnes[strNum.charAt(2)]);
+    break;
+
+    case 2:
+    romanNumeralsKeeper.push(romanTens[strNum.charAt(0)]); 
+    romanNumeralsKeeper.push(romanOnes[strNum.charAt(1)]);
+    break;
+
+    case 1:
+    romanNumeralsKeeper.push(romanOnes[strNum.charAt(2)]);  
+    break; 
+
+} 
+
+
+number = romanNumeralsKeeper.join("");
+   console.log(number);
+    return number;
    }
    
-   convertToRoman(5);
+   convertToRoman(3500);
